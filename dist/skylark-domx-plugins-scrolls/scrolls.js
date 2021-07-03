1a33fb64ffe5533512876a90c0df8e901a9f9a6a
hudaokeji <github@hudaokeji.com>
2021-07-03 18:50:43 +0900

    update

---
+++ dist/skylark-domx-plugins-scrolls/scrolls.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-domx-plugins-scrolls - The skylark scrolls plugins library for dom api extension
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-domx-plugins/skylark-domx-plugins-scrolls/
+ * @license MIT
+ */
+define(["skylark-langx/skylark"],function(l){return l.attach("domx.plugins.scrolls",{})});
+//# sourceMappingURL=sourcemaps/scrolls.js.map
---
+++ dist/uncompressed/skylark-domx-plugins-scrolls/scrolls.js
@@ -0,0 +1,8 @@
+define([
+  "skylark-langx/skylark",
+],function(skylark){
+
+
+	return skylark.attach("domx.plugins.scrolls",{});
+
+});
